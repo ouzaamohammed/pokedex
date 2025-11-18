2b9a6cabb3044f9703208b252b8555e6b1725b1e
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-11-18 18:34:49 +0100

    create a simple REPL that print first word of the input

--- src/repl.ts
+++ src/repl.ts
@@ -1,7 +1,30 @@
+import { createInterface } from "node:readline";
+import { stdin, stdout } from "node:process";
+
 export function cleanInput(input: string): string[] {
   return input
     .toLowerCase()
     .trim()
     .split(" ")
     .filter((word) => word !== "");
+}
+
+export function startREPL() {
+  const rl = createInterface({
+    input: stdin,
+    output: stdout,
+    prompt: "Pokedex > ",
+  });
+
+  rl.prompt();
+  rl.on("line", (input) => {
+    const words = cleanInput(input);
+    if (!input) {
+      rl.prompt();
+      return;
+    }
+
+    console.log(`Your command was: ${words[0]}`);
+    rl.prompt();
+  });
 }
